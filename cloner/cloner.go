19b6b0aed12cfb720448a9cce4eff1c65067b772
Mai Hoang Hai <hoanghai27@gmail.com>
2015-12-05 17:56:59 +0700

    init code template

---
+++ cloner/cloner.go
@@ -0,0 +1,20 @@
+package cloner
+
+import (
+	"fmt"
+	//"regexp"
+	//"net/http"
+	"path/filepath"
+)
+
+func Start(url string, outDir string) {
+	outDir, _ = filepath.Abs(outDir)
+	fmt.Printf("Clone book from %s to %s? (yes/no): ", url, outDir)
+	var choise string
+	fmt.Scanf("%s", choise)
+	if choise == "yes" {
+		fmt.Printf("Just kidding :D, we'll handle this function later!\n")
+	} else {
+		fmt.Printf("User aborted!\n")
+	}
+}
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"os"
+	"github.com/hoanghai27/bookcloner/cloner"
+)
+
+func main() {
+	command := os.Args[1]
+
+	if command == "help" || command == "--help" || command == "-h" {
+		showHelp();
+	} else {
+		valid, url, outDir := getParams()
+		if valid {
+			cloner.Start(url, outDir);
+		}
+	}
+}
+
+func getParams() (bool, string, string) {
+	url := os.Args[1]
+	outDir := "output";
+
+	if linkValid(url) == false {
+		fmt.Printf("URL \"%s\" is not valid.\n", url);
+		fmt.Printf("Valid URL must be like this: http://truyenyy.com/truyen/ten-truyen/.\n");
+		fmt.Printf("For help, please enter: bookcloner help.\n");
+		return false, "", "";
+	}
+
+	if len(os.Args) >= 3 {
+		outDir = os.Args[2];
+	}
+
+	return true, url, outDir;
+}
+
+func linkValid(url string) bool {
+	valid, _ := regexp.MatchString("http://truyenyy.com/truyen/[A-Za-z-0-9]/", url)
+	return valid
+}
+
+func showHelp() {
+	fmt.Printf("Usage: bookcloner link [outputDir]\n")
+	fmt.Printf("Params: \n")
+	fmt.Printf("- link: Book url from truyenyy.com, example: http://truyenyy.com/truyen/ten-truyen/\n")
+	fmt.Printf("- outputDir (optional): Output path\n")
+	fmt.Printf("Example:  bookcloner http://truyenyy.com/truyen/ac-ma-phap-tac/ am-ma-phap-tac\n")
+}
